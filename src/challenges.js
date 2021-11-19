9d0a73a2015418982b4ea03daf788ede66d90e3c
Isabela Correia <isabela.correiasilva00@gmail.com>
2021-11-18 23:10:34 -0300

    }Lint no challenge.js e desafio 11/12 no challenge2.js

--- src/challenges.js
+++ src/challenges.js
@@ -1,170 +1,143 @@
 // Desafio 1
-
-console.log(compareTrue(1, 1));
 function compareTrue(boolOne, boolTwo) {
-  // if(boolOne && boolTwo){
-  //   return(true);
-  // }else{
-  //   return(false);
-  // }
-
-  return(boolOne && boolTwo)
+  return (boolOne && boolTwo);
 }
 
 // Desafio 2
 function calcArea(base, height) {
-  let area = (base*height)/2;
-  return(area);
+  let area = (base * height) / 2;
+  return (area);
 }
 
 // Desafio 3
 // Fonte de pesquisa: https://www.w3schools.com/jsref/jsref_split.asp
 function splitSentence(text) {
   // A função .split separa uma string em sub-strings e retorna um array com a string original separada.
-  let resultado = text.split(" ");
-  return(resultado);
+  let resultado = text.split('');
+  return (resultado);
 }
 
 // Desafio 4
 function concatName(array) {
-  return(array.pop() + ', ' + array.shift());
+  return (array.pop() + ', ' + array.shift());
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  let resultado = (wins*3)+(ties);
-  return(resultado);
+  let resultado = (wins * 3) + (ties);
+  return (resultado);
 }
 
 // Desafio 6
 function highestCount(array) {
-
   let contagem = 0;
 
-  // Definindo a variável "temp" como a primeira posição do array para que caso um array de números negativos 
-  //seja entrado, o valor padrão (0) da variável vazia não interfira na comparação.
+  // Definindo a variável "temp" como a primeira posição do array para que caso um array de números negativos seja entrado, o valor padrão (0) da variável vazia não interfira na comparação.
   let temp = array[0];
-
-  for(i = 0; i < array.length; i += 1){
-    if(temp < array[i]){
+  for (let i = 0; i < array.length; i += 1) {
+    if (temp < array[i]) {
       temp = array[i];
     }
   }
-
-  for(i = 0; i < array.length; i += 1){
-    if(array[i] === temp){
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] === temp) {
       contagem += 1;
-    } 
-  } 
-
-  return(contagem);
+    }
+  }
+  return (contagem);
 }
 
-// Desafio 7 
+// Desafio 7
 // Para conseguir a diferença absoluta entre as distâncias, ref:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
 function catAndMouse(mouse, cat1, cat2) {
-
   let cat1Dist = Math.abs(cat1 - mouse);
   let cat2Dist = Math.abs(cat2 - mouse);
 
-  if(cat1Dist < cat2Dist){
-    return('cat1');
-  }else if(cat2Dist < cat1Dist){
-    return('cat2');
-  }else{
-    return('os gatos trombam e o rato foge')
+  if (cat1Dist < cat2Dist) {
+    return ('cat1');
+  } else if (cat2Dist < cat1Dist) {
+    return ('cat2');
   }
+  return ('os gatos trombam e o rato foge');
 }
 
 // Desafio 8
 function fizzBuzz(array) {
   let resultado = [];
-  for(i=0; i<array.length; i+=1){
-
-    if(array[i]%3==0 || array[i]%5==0){
-
-      if(array[i]%3==0 && array[i]%5!==0){
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] % 3 === 0 || array[i] % 5 === 0) {
+      if (array[i] % 3 === 0 && array[i] % 5 !== 0) {
         resultado.push('fizz');
-
-      }else if(array[i]%5==0 && array[i]%3!==0){
-        resultado.push('buzz'); 
-
-      }else if(array[i]%3==0 && array[i]%5==0){
+      } else if (array[i] % 5 === 0 && array[i] % 3 !== 0) {
+        resultado.push('buzz');
+      } else if (array[i] % 3 === 0 && array[i] % 5 === 0) {
         resultado.push('fizzBuzz');
-        
       }
-
-    }else{
+    } else {
       resultado.push('bug!');
     }
   }
-    return(resultado);
+  return (resultado);
 }
 
-
 // Desafio 9
-
 function encode(originalMessage) {
-
-  //percorrer string, separar cada caractere num array
+  // percorrer string, separar cada caractere num array
   let arrayOriginMes = [];
-  for(let i = 0; i<originalMessage.length; i+=1){
-    arrayOriginMes = originalMessage.split("");
+  for (let i = 0; i < originalMessage.length; i += 1) {
+    arrayOriginMes = originalMessage.split('');
   }
 
   // comparar cada caractere, substituir vogais por numeros
-  for(let i = 0; i<arrayOriginMes.length; i += 1){
-    if(arrayOriginMes[i] == 'a'){
+  for (let i = 0; i < arrayOriginMes.length; i += 1) {
+    if (arrayOriginMes[i] === 'a') {
       arrayOriginMes[i] = '1';
-    }else if(arrayOriginMes[i] == 'e'){
+    } else if (arrayOriginMes[i] === 'e') {
       arrayOriginMes[i] = '2';
-    }else if(arrayOriginMes[i] == 'i'){
+    } else if (arrayOriginMes[i] === 'i') {
       arrayOriginMes[i] = '3';
-    }else if(arrayOriginMes[i] == 'o'){
+    } else if (arrayOriginMes[i] === 'o') {
       arrayOriginMes[i] = '4';
-    }else if(arrayOriginMes[i] == 'u'){
+    } else if (arrayOriginMes[i] === 'u') {
       arrayOriginMes[i] = '5';
     }
   }
-  
+
   // Reconstruir(concatenando) a string a partir do Array já codificado
   let codedMessage = '';
-  for(let i = 0; i<arrayOriginMes.length; i += 1){
-    codedMessage = codedMessage + arrayOriginMes[i];
+  for (let i = 0; i < arrayOriginMes.length; i += 1) {
+    codedMessage += arrayOriginMes[i];
   }
-  return(codedMessage);
-
+  return (codedMessage);
 }
 
-// Basicamente uma cópia do código da função encode. 
+// Basicamente uma cópia do código da função encode.
 // Mudanças: nomes de variáveis, e a troca de números para vogais
 function decode(message) {
   let arrayMes = [];
-
-  for(let i = 0; i<message.length; i+=1){
-    arrayMes = message.split("");
+  for (let i = 0; i < message.length; i += 1) {
+    arrayMes = message.split('');
   }
 
-  for(let i = 0; i<arrayMes.length; i += 1){
-    if(arrayMes[i] == '1'){
+  for (let i = 0; i < arrayMes.length; i += 1) {
+    if (arrayMes[i] === '1') {
       arrayMes[i] = 'a';
-    }else if(arrayMes[i] == '2'){
+    } else if (arrayMes[i] === '2') {
       arrayMes[i] = 'e';
-    }else if(arrayMes[i] == '3'){
+    } else if (arrayMes[i] === '3') {
       arrayMes[i] = 'i';
-    }else if(arrayMes[i] == '4'){
+    } else if (arrayMes[i] === '4') {
       arrayMes[i] = 'o';
-    }else if(arrayMes[i] == '5'){
+    } else if (arrayMes[i] === '5') {
       arrayMes[i] = 'u';
     }
   }
-  
   let decodedMessage = '';
-  for(let i = 0; i<arrayMes.length; i += 1){
-    decodedMessage = decodedMessage + arrayMes[i];
+  for (let i = 0; i < arrayMes.length; i += 1) {
+    decodedMessage += arrayMes[i];
   }
-  return(decodedMessage);
+  return (decodedMessage);
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,17 +1,64 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+
+  if (array.length === 0){
+    return ('Vazio!');    
+  }  
+  let lista = [];
+  let ordenado = array.sort();
+
+  for (let i = 0; i < ordenado.length; i += 1) {
+    lista.push(
+      {
+        tech: ordenado[i], 
+        name: name, 
+      }, 
+    );
+  }
+  return lista;
 }
+
+console.log( techList(["React", "Jest", "HTML", "CSS", "JavaScript"],"Lucas") );
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numero) {
+  if(numero.length != 11) {
+    return 'Array com ramanho incorreto.'
+    // OK
+  }
+
+  for ( let i = 0; i < numero.length; i += 1) {
+    if (numero[i] < 0 || numero[i] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+      // OK
+    }
+    let cont = 0;
+    for (let j = 1; j<numero.length; j += 1){
+
+      if (numero[i] === numero[j]){
+        cont =+ 1;
+      }
+
+      if ( cont >= 3){
+        return 'não é possível gerar um número de telefone com esses valores';
+        // OK
+      }
+    }
+  }
 }
 
+console.log( generatePhoneNumber([1, 2, 3, 4, 1, 1, 7, 8, 9, 0, 1])  );
+
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA + lineB > lineC && lineC > Math.abs(lineA - lineB)){
+    return true;
+  }
+  return false;
+
 }
+
+console.log( triangleCheck(10, 14, 8) );
 
 // Desafio 13
 // refs:
@@ -27,16 +74,25 @@ function hydrate(bebidas) {
   let numBebidasArray = bebidas.match(digitos).map(Number);
 
   let qtdBebidas = 0;
-  for(let i = 0; i < numBebidasArray.length; i += 1){
-    qtdBebidas = qtdBebidas + numBebidasArray[i];
+  for (let i = 0; i < numBebidasArray.length; i += 1) {
+    qtdBebidas += numBebidasArray[i];
   }
 
-  if(qtdBebidas > 1){
-    return(qtdBebidas + ' copos de água')
-  }else{
-    return(qtdBebidas + ' copo de água')
+  if (qtdBebidas === 1) {
+    return (qtdBebidas + ' copo de água');
   }
+  return (qtdBebidas + ' copos de água');
 }
+
+// function hydrate2(bebidas) {
+//   let numBebidas = parseInt(bebidas, 10);
+
+//   for (let i = 0; i < bebidas.length; i += 1) {
+//     console.log( parseInt(bebidas[i]) );
+//   }
+// }
+
+// console.log( hydrate2('1 vihno') );
 
 module.exports = {
   generatePhoneNumber,
